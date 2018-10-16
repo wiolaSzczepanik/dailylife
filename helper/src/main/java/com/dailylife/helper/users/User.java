9b6892ba1c03842cdc03e43d6752c3e2d4e5e8e8
wiolaSzczepanik <wiolaszcz@gmail.com>
2018-10-16 13:46:22 +0200

    implement simply user model

--- helper/src/main/java/com/dailylife/helper/users/User.java
+++ helper/src/main/java/com/dailylife/helper/users/User.java
@@ -1,4 +1,58 @@
 package com.dailylife.helper.users;
 
+import java.util.Objects;
+
 public class User {
+
+    private String id;
+    private String firstname;
+    private String lastname;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) &&
+                Objects.equals(firstname, user.firstname) &&
+                Objects.equals(lastname, user.lastname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstname, lastname);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                '}';
+    }
 }
